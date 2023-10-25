117278a4b1e7a9af6c0b7cfd115b9beb1202b066
Roomsrio <roomsrio7782@gmail.com>
2023-10-25 23:01:55 +0200

    feat(ListItem): add style and structure

--- App/Components/Lists/ListItem.js
+++ App/Components/Lists/ListItem.js
@@ -1,38 +1,201 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import React from "react";
+import colors from "../../colors";
+import { Entypo } from "@expo/vector-icons";
 
-export default function ListItem({ listId }) {
-    const listItems = [
-        {
-            name: 'Lait',
-            list_id: 1,
-            account_id: 1,
-            agenda_id: 1,
-        },
-        {
-            name: 'Pain',
-            list_id: 1,
-            account_id: 2,
-            agenda_id: 1,
-        },
-        {
-            name: 'peinture',
-            list_id: 2,
-            account_id: 2,
-            agenda_id: 2,
-        },
-    ];
-    return (
-        <View>
+export default function ListItem({ list }) {
+  const listItems = [
+    {
+      id: 1,
+      name: "Lait",
+      list_id: 1,
+      account_id: 1,
+      agenda_id: 1,
+    },
+    {
+      id: 2,
+      name: "Pain",
+      list_id: 1,
+      account_id: 2,
+      agenda_id: 1,
+    },
+    {
+      id: 3,
+      name: "Dentifrice avec de la menthe",
+      list_id: 1,
+      account_id: 2,
+      agenda_id: 1,
+    },
+    {
+      id: 2,
+      name: "Pain",
+      list_id: 1,
+      account_id: 2,
+      agenda_id: 1,
+    },
+    {
+      id: 3,
+      name: "Dentifrice avec de la menthe",
+      list_id: 1,
+      account_id: 2,
+      agenda_id: 1,
+    },
+    {
+      id: 2,
+      name: "Pain",
+      list_id: 1,
+      account_id: 2,
+      agenda_id: 1,
+    },
+    {
+      id: 3,
+      name: "Dentifrice avec de la menthe",
+      list_id: 1,
+      account_id: 2,
+      agenda_id: 1,
+    },
+    {
+      id: 2,
+      name: "Pain",
+      list_id: 1,
+      account_id: 2,
+      agenda_id: 1,
+    },
+    {
+      id: 3,
+      name: "Dentifrice avec de la menthe",
+      list_id: 1,
+      account_id: 2,
+      agenda_id: 1,
+    },
+    {
+      id: 2,
+      name: "Pain",
+      list_id: 1,
+      account_id: 2,
+      agenda_id: 1,
+    },
+    {
+      id: 3,
+      name: "Dentifrice avec de la menthe",
+      list_id: 1,
+      account_id: 2,
+      agenda_id: 1,
+    },
+    {
+      id: 2,
+      name: "Pain",
+      list_id: 1,
+      account_id: 2,
+      agenda_id: 1,
+    },
+    {
+      id: 3,
+      name: "Dentifrice avec de la menthe",
+      list_id: 1,
+      account_id: 2,
+      agenda_id: 1,
+    },
+    {
+      id: 2,
+      name: "Pain",
+      list_id: 1,
+      account_id: 2,
+      agenda_id: 1,
+    },
+    {
+      id: 3,
+      name: "Dentifrice avec de la menthe",
+      list_id: 1,
+      account_id: 2,
+      agenda_id: 1,
+    },
+    {
+      id: 2,
+      name: "Pain",
+      list_id: 1,
+      account_id: 2,
+      agenda_id: 1,
+    },
+    {
+      id: 3,
+      name: "Dentifrice avec de la menthe",
+      list_id: 1,
+      account_id: 2,
+      agenda_id: 1,
+    },
+    {
+      id: 2,
+      name: "Pain",
+      list_id: 1,
+      account_id: 2,
+      agenda_id: 1,
+    },
+    {
+      id: 2,
+      name: "Pain",
+      list_id: 1,
+      account_id: 2,
+      agenda_id: 1,
+    },
+    {
+      id: 3,
+      name: "peinture",
+      list_id: 2,
+      account_id: 2,
+      agenda_id: 2,
+    },
+  ];
 
-            {
-                listItems.filter(item => item.list_id === listId).map((item, index) => (
-                    <View key={index}>
-                        <Text>{item.name}</Text>
-                    </View>
-                ))
-            }
-        </View>
+  const styles = StyleSheet.create({
+    itemContainer: {
+      backgroundColor: colors.light,
+      paddingTop: 50,
+      flexGrow: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      gap: 10,
+    },
+    item: {
+      flexDirection: "row",
+      borderRadius: 5,
+      justifyContent: "space-between",
+      backgroundColor: colors.quaternary,
+      borderColor: colors.tertiary,
+      borderStyle: "solid",
+      borderWidth: 1,
+      padding: 10,
+      width: 300,
+    },
+    textItem: {
+      color: colors.primary,
+      fontSize: 15,
+    },
+  });
 
-    )
+  const handleDeleteItem = (item) => {
+    console.log("delete item" + item.id);
+  };
+
+  return (
+    <ScrollView contentContainerStyle={styles.itemContainer}>
+      <View>
+        <Text>{list.name}</Text>
+        <Entypo name="trash" size={24} color="black" />
+      </View>
+      {listItems
+        .filter((item) => item.list_id === list.id)
+        .map((item, index) => (
+          <View key={index} style={styles.item}>
+            <Text style={styles.textItem}>{item.name}</Text>
+            <Entypo
+              onPress={() => handleDeleteItem(item)}
+              name="trash"
+              size={24}
+              color="black"
+            />
+          </View>
+        ))}
+    </ScrollView>
+  );
 }
