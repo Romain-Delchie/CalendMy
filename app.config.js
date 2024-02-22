3b45be23dc45b512176aa7a17ee65fe153f64d53
Romain-Delchie <roomsrio7782@gmail.com>
2024-02-22 10:39:36 +0100

    fix build.gradle

--- app.config.js
+++ app.config.js
@@ -2,7 +2,32 @@ module.exports = {
   name: "CalendMy",
   slug: "CalendMy",
   version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+  assetBundlePatterns: ["**/*"],
+  ios: {
+    supportsTablet: true,
+  },
+  android: {
+    package: "com.roomsrio.calendmy",
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#ffffff",
+    },
+  },
+  web: {
+    favicon: "./assets/favicon.png",
+  },
   extra: {
     clerkPublishableKey: process.env.CLERK_PUBLISHABLE_KEY,
+    eas: {
+      projectId: "bd6ba29d-f61f-4663-a7ad-67ba9e87870e",
+    },
   },
 };
